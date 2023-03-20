87a62e516033e213631c48e0150c2aee26da3659
Taylor Pridgen <musicinsilence13@gmail.com>
2023-03-19 19:55:47 -0600

    slash/ health commands and gpt fallback

--- modules/openai/converse.ts
+++ modules/openai/converse.ts
@@ -22,8 +22,10 @@ export const chatWithUser = async (user: string, msg: string) => {
   let personality;
   let chat = msg;
 
+  // Easier to work with
   const convertedMsg = msg.toLowerCase().trim();
 
+  // Check if we're including a personality for this
   if (convertedMsg.startsWith("as") && convertedMsg.includes(":")) {
     const msgParts = convertedMsg.split(":");
     personality = msgParts[0].replace("as", "").trim();
@@ -32,6 +34,7 @@ export const chatWithUser = async (user: string, msg: string) => {
 
   const prompt = chat.replace("hey fini", "");
 
+  // Setup message object for the personality
   const startingMessage: ChatCompletionRequestMessage = {
     content: personality
       ? `You are ${personality}`
@@ -48,20 +51,28 @@ export const chatWithUser = async (user: string, msg: string) => {
     content: prompt,
   });
 
+  // Transforms message history into chat message objects
   const conversationMessages: ChatCompletionRequestMessage[] =
     conversationHistory[user]?.map((context) => ({
       content: context.content,
       role: context.role,
     })) || [];
 
+  // Hit the api
   try {
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [startingMessage, ...conversationMessages],
-      user: user,
-    });
+    // Combine the personality, conversation history, and latest prompt into one array
+    const combinedMessages = [startingMessage, ...conversationMessages];
+
+    // Call api and wait for response
+    let response = await getChatResponse(combinedMessages, openai, "gpt-4");
+
+    if (response.status === 429) {
+      // We got rate limited, try again with 3.5
+      response = await getChatResponse(combinedMessages, openai, "gpt-3.5");
+    }
 
     if (response.status === 200) {
+      // Everything is good, let's grab a random response
       const choices = response?.data?.choices;
       const rand = Math.round(Math.random() * (choices.length - 1));
 
@@ -79,9 +90,26 @@ export const chatWithUser = async (user: string, msg: string) => {
       // Display message to user in chat
       return choices[rand].message?.content || "";
     } else {
+      // Something went wrong
       return "Something fucked up D:";
     }
   } catch (err) {
+    // Something went very wrong
     return `Something fucked up D: (${err})`;
   }
+};
+
+// helper function to call the api
+const getChatResponse = async (
+  messages: ChatCompletionRequestMessage[],
+  openai: OpenAIApi,
+  model: "gpt-4" | "gpt-3.5"
+) => {
+  console.log("Calling OpenAI Chat Completion with:", { messages, model });
+  const response = await openai.createChatCompletion({
+    model,
+    messages,
+  });
+
+  return response;
 };
--- modules/polling/polling.ts
+++ modules/polling/polling.ts
@@ -1,11 +1,14 @@
 import { db } from "./../../utilities";
-import { ReminderRecord } from "./../../types";
+import { ReminderRecord, SettingsRecord } from "./../../types";
 import { Client, TextChannel } from "discord.js";
-import { run } from "./../../commands/feed.command";
+import { run as runFeeds } from "./../../commands/feed.command";
+import { chatWithUser } from "./../openai";
+import { HEALTH_KEY } from "./../../commands/health.command";
 
 export const runPollTasks = (cl: Client) => {
   checkReminders(cl);
   checkFeeds(cl);
+  checkHealthPings(cl);
 };
 
 /* Polling Functions to run */
@@ -49,6 +52,53 @@ const checkFeeds = (cl: Client) => {
     today.getHours() === hourToCheck &&
     today.getMinutes() === minuteToCheck
   ) {
-    run(undefined, cl);
+    runFeeds(undefined, cl);
+  }
+};
+
+// Check for health reminders
+const checkHealthPings = async (cl: Client) => {
+  const today = new Date();
+
+  // Check at these times
+  const hourToCheck = [10, 13, 16, 20];
+  const minuteToCheck = 15;
+
+  const currentHour = today.getHours();
+  const currentMinute = today.getMinutes();
+
+  if (currentMinute === minuteToCheck && hourToCheck.includes(currentHour)) {
+    console.log("Running health poll");
+    // Get the health message
+    const healthMessage = await chatWithUser(
+      "",
+      "Please provide a quick healthy activity I can do in my office. Please keep the activity to about a minute in length. Please provide the activity in the form of a JSON object, with the keys being: 'activity_name', 'acitivity_description', 'acitivity_benefits'"
+    );
+    console.log({ healthMessage, JSONified: JSON.parse(healthMessage) });
+    try {
+      const activityJSON = JSON.parse(healthMessage);
+
+      // We have our health message
+      const channels = await db().select<SettingsRecord>("Settings", {
+        Field: "Key",
+        Value: HEALTH_KEY,
+      });
+
+      channels.forEach(async (ch) => {
+        const healthChannel = await cl.channels.fetch(ch.Value);
+        (healthChannel as TextChannel).send(
+          `Friendly reminder to do something healthy!`
+        );
+        (healthChannel as TextChannel).send(
+          `**${activityJSON.activity_name}**`
+        );
+        (healthChannel as TextChannel).send(activityJSON.acitivity_description);
+      });
+    } catch (err) {
+      console.error("Error parsing openai response", {
+        err,
+        openaiResponse: healthMessage,
+      });
+    }
   }
 };
