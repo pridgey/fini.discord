a58416fd00c190106789aeaf2b0478ed2751d634
Taylor Pridgen <taylor@pridgey.dev>
2024-02-24 14:38:27 -0700

    main/ better string splitting

--- utilities/splitBigString.ts
+++ utilities/splitBigString.ts
@@ -1,23 +1,33 @@
 /**
  * Breaks large strings into an array of smaller strings -- helpful for things like Discord with max message sizes
  * @param str The string to split into multiple strings
- * @param addEllipsis Whether or not to add a trailing ellipsis to the prior strings
  * @param maxLength The max length of any given string
  * @returns array of smaller strings
  */
-export const splitBigString = (
-  str: string,
-  maxLength: number = 1990
-): string[] => {
-  // If less than max amount, simply return
+export const splitBigString = (str: string, maxLength: number = 1990): string[] => {
   if (str.length <= maxLength) {
     return [str];
   }
 
-  // Gets a chunk of string
-  const stringChunk = str.slice(0, maxLength);
+  const markdownRegexes: RegExp[] = [
+    /\*\*[^]*?\*\*/,      // Bold markdown
+    /\*[^]*?\*/,          // Single star Italic
+    /__[^]*?__/,          // Double line Bold
+    /_[^]*?_/,            // Italic markdown
+    /`[^]*?`/,            // Inline code markdown
+    /```[^]*?```/,        // Code block markdown
+    /\n/                  // Newline character
+  ];
 
-  // Recursively goes through the rest
-  const remainingString = splitBigString(str.slice(maxLength), maxLength);
-  return [stringChunk, ...remainingString];
+  for (const regex of markdownRegexes) {
+    const match = regex.exec(str.slice(0, maxLength + 1));
+    if (match && match.index !== undefined) {
+      if (regex.source === '\n') { // If the match is a newline character
+        return [str.slice(0, maxLength), ...splitBigString(str.slice(maxLength + 1), maxLength)];
+      }
+      return [str.slice(0, maxLength + match.index + (regex.source === '```' ? 6 : 2)), ...splitBigString(str.slice(maxLength + match.index + (regex.source === '```' ? 6 : 2)), maxLength)];
+    }
+  }
+
+  return [str.slice(0, maxLength), ...splitBigString(str.slice(maxLength), maxLength)];
 };
