3b8da3f906a099a0a9f6a7facd2cfc7a754ce8bf
Taylor Pridgen <taylor@pridgey.dev>
2025-01-26 22:56:40 -0700

    main/ script to review current set images

--- scripts/review_set.ts
+++ scripts/review_set.ts
@@ -3,6 +3,7 @@ import { createCardImage } from "../modules/finicards/generateCardImage";
 import { CardDefinitionRecord } from "../types/PocketbaseTables";
 import { pb } from "../utilities/pocketbase";
 import path from "path";
+import { exists } from "fs/promises";
 
 const previewSet = async (set: number) => {
   const setCards = await pb
@@ -12,13 +13,19 @@ const previewSet = async (set: number) => {
     });
 
   for (let i = 0; i < setCards.length; i++) {
-    const cardBuffer = await createCardImage(setCards[i]);
+    const fileName = path.join(
+      __dirname,
+      "test_images",
+      `${setCards[i].rarity}-${setCards[i].card_name}-${setCards[i].id}.png`
+    );
 
-    await sharp(cardBuffer)
-      .png()
-      .toFile(
-        path.join(__dirname, "test_images", `${setCards[i].card_name}.png`)
-      );
+    const fileExists = await exists(fileName);
+
+    if (!fileExists) {
+      const cardBuffer = await createCardImage(setCards[i]);
+
+      await sharp(cardBuffer).png().toFile(fileName);
+    }
   }
 };
 
